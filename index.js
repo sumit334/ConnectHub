77c1cb085451fa486b0648fe560e3c2ea079b07d
Sumit Agarwal <97662774+sumit334@users.noreply.github.com>
2023-06-27 00:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors=require("cors");
-
+//backend
 const io= require("socket.io")(server,{
     cors:{
         origin:"*",
